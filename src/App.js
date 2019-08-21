0001b967ca10e21cde71d4eb36f7ba135af4f1ff
unknown <Marcusnyman556@gmail.com>
2019-08-21 13:43:59 +0200

    First commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { robots } from "./robots";
+import CardList from './CardList'
+
+
+const App = () => {
+  return (
+    <CardList robots={robots} />
+  );
+}
+
+export default App;
